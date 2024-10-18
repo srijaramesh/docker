42985116413744d1c773035f09a88f1487453d57
radha srija <srijaramesh>
2024-10-18 18:08:27 +0530

    Initial commit with express app and Dockerfile

---
+++ app.js
@@ -0,0 +1,26 @@
+// const express=require('express');
+// const app=express();
+// const port=3000;
+// app.get("/",(req,res)=>
+
+//   {
+//     res.send("<h1>hello world</h1>");
+
+// });
+// app.listen(port,()=>
+// {
+//   console.log('app listeneing to the port ${port}');
+// }
+// )
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);  // Corrected with backticks
+});
+// http://localhost:3000
